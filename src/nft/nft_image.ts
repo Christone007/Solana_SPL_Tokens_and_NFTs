b7d794fae620bc8a605f1947878d0e6b8a3eb711
Christone007 <exellentemy@gmail.com>
2026-09-02 04:10:23 +0100

    Upload images to decentralized cloud

---
+++ src/nft/nft_image.ts
@@ -0,0 +1,47 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
+
+import wallet from "../../devnet-wallet.json";
+
+const umi = createUmi(
+  process.env.SOLANA_RPC_URL ?? "https://api.devnet.solana.com",
+);
+
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(
+  irysUploader({
+    address: "https://devnet.irys.xyz/",
+  }),
+);
+
+umi.use(signerIdentity(signer));
+
+(async () => {
+  try {
+    //chanege image path to your image path
+    // const image = await readFile("/home/christone/Turbin3/spl-nft-q326/src/nft/generug.png");
+    const image = await readFile("/home/christone/Turbin3/spl-nft-q326/src/nft/christonemobile.png");
+
+
+    //change the image name and mime type
+    // const file = createGenericFile(image, "Christone", {contentType: "image/png"});
+    const file = createGenericFile(image, "ChristoneMobile", {contentType: "image/png"});
+
+    const [myUri] = await umi.uploader.upload([file]);
+
+    console.log("Your image URI: ", myUri);
+
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+// SPL Token: Your image URI:  https://gateway.irys.xyz/7tnMzY1QcrUMk159TWL9USdsvDoMEYAx7McY2sdTMtBv
